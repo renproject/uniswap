df4c13192d8650cec7c419f2acb511cf9af4cea1
susruth <susruth@republicprotocol.com>
2019-06-20 16:24:39 +1000

    contracts: Add uniswap and uniswap adapter contracts

---
+++ contracts/UniswapAdapterFactory.sol
@@ -0,0 +1,48 @@
+pragma solidity 0.5.9;
+
+import "./UniswapExchangeAdapter.sol";
+import "./UniswapReserveAdapter.sol";
+import "darknode-sol/contracts/Shifter/Shifter.sol";
+import "darknode-sol/contracts/Shifter/ShifterRegistry.sol";
+
+interface UniswapFactory {
+    function createExchange(address token) external returns (address);
+    function getExchange(address token) external view returns (address);
+}
+
+contract UniswapAdapterFactory {
+    UniswapFactory factory;
+    ShifterRegistry registry;
+
+    mapping (address=>UniswapExchangeAdapter) exchangeAdapters;
+    mapping (address=>UniswapReserveAdapter) reserveAdapters;
+
+    constructor(UniswapFactory _factory, ShifterRegistry _registry) public {
+        factory = _factory;
+        registry = _registry;
+    }
+
+    function createExchange(address _token) external 
+        returns 
+            (
+                address exchange,
+                UniswapExchangeAdapter exchangeAdapterAddress,
+                UniswapReserveAdapter exchangeReserveAddress
+            ) 
+        {
+        exchange = factory.createExchange(_token);
+        Shifter shifter = Shifter(registry.getShifter(_token));
+        exchangeAdapterAddress = new UniswapExchangeAdapter(UniswapExchange(exchange), shifter);
+        exchangeAdapters[_token] = exchangeAdapterAddress;
+        exchangeReserveAddress = new UniswapReserveAdapter(UniswapReserve(exchange), shifter);
+        reserveAdapters[_token] = exchangeReserveAddress;
+    }
+
+    function getExchangeAdapter(address _token) external view returns (UniswapExchangeAdapter) {
+        return exchangeAdapters[_token];
+    }
+
+    function getReserveAdapter(address _token) external view returns (UniswapReserveAdapter) {
+        return reserveAdapters[_token];
+    }
+}
---
+++ contracts/UniswapReserveAdapter.sol
@@ -0,0 +1,77 @@
+pragma solidity 0.5.9;
+
+import "darknode-sol/contracts/Shifter/Shifter.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/IERC20.sol";
+
+contract UniswapReserveAdapter {
+    UniswapReserve public reserve;  
+    Shifter public shifter;  
+    constructor(UniswapReserve _reserve, Shifter _shifter) public {
+        reserve = _reserve;
+        shifter = _shifter;
+    }
+
+    function addLiquidity(
+        uint256 _amount, bytes32 _nHash, bytes calldata _sig,
+        uint256 _minLiquidity, uint256 _deadline, bytes calldata _refundAddress
+        ) 
+            external 
+            payable 
+            returns (uint256  uniMinted) 
+        {
+        bytes32 pHash = keccak256(abi.encode(_minLiquidity, _deadline, _refundAddress));
+        uint256 amount = shifter.shiftIn(_amount, _nHash, pHash, _sig);
+
+        if (block.number > _deadline) {
+            shifter.shiftOut(_refundAddress, amount);
+            return 0;
+        }
+
+        IERC20 token = IERC20(reserve.token());
+        token.approve(address(reserve), _amount);
+        uniMinted = reserve.addLiquidity.value(msg.value)(_minLiquidity, amount, _deadline);
+
+        uint256 balance = token.balanceOf(address(this));
+        if ( balance > 0 ) {
+            shifter.shiftOut(_refundAddress, balance);
+        }
+        return uniMinted;
+    }
+
+    function removeLiquidity(
+            uint256 _uniAmount, uint256 _minEth, uint256 _minTokens,
+            uint256 _deadline, bytes calldata _to
+        ) 
+            external 
+            payable 
+            returns (uint256  eth, uint256 tokens)
+        {
+        (uint256 ethAmount, uint256 tokenAmount) = reserve.removeLiquidity(_uniAmount, _minEth, _minTokens, _deadline);
+        msg.sender.transfer(ethAmount);
+        uint256 shiftedAmount = shifter.shiftOut(_to, tokenAmount);
+        return (ethAmount, shiftedAmount);
+    }
+}
+
+interface UniswapReserve {
+    function addLiquidity(
+        uint256 min_liquidity, 
+        uint256 max_tokens,
+        uint256 deadline
+    )   
+        external
+        payable
+        returns (uint256  uni_minted);
+
+    function removeLiquidity(
+        uint256 amount, 
+        uint256 min_eth,
+        uint256 min_tokens,
+        uint256 deadline
+    )   
+        external
+        payable
+        returns (uint256  eth, uint256 tokens);
+
+    function token() external view returns (address);
+}
