c175ff765c87aa14e94cc55f5501860cdb3ee5ab
susruth <susruth@republicprotocol.com>
2019-06-25 16:37:34 +1000

    migrations: updated to deploy uniswap adapter factories

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -1,7 +1,17 @@
 /// <reference types="../types/truffle-contracts" />
 
-const Example = artifacts.require("Example");
+const UniswapAdapterFactory = artifacts.require("UniswapAdapterFactory");
+
+const devnetShifterRegistry = "0xA28cC8B81906D2A42beF0bF782CECe3b75f91E6b"
+const testnetShifterRegistry = "0x89aB0D4e64b1cb7F961228b70595a46BF0761546"
+const kovanUniswapFactory = "0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30"
 
 module.exports = async function (deployer, network) {
-    await deployer.deploy(Example);
+    if (network === "kovan") {
+        const devnetUniswapAdapterFactory = await deployer.deploy(UniswapAdapterFactory, kovanUniswapFactory, devnetShifterRegistry);
+        const testnetUniswapAdapterFactory = await deployer.deploy(UniswapAdapterFactory, kovanUniswapFactory, testnetShifterRegistry);
+
+        console.log("Devnet UniswapAdapterFactory: ", devnetUniswapAdapterFactory.address);
+        console.log("Testnet UniswapAdapterFactory: ", testnetUniswapAdapterFactory.address);
+    }
 }
